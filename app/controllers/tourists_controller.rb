919ebf485826441a3aa85161db7fab4e57d34dff
Leif Bersamina <leifbersamina@gmail.com>
2021-07-15 19:23:02 +0800

    modify templates

--- app/controllers/tourists_controller.rb
+++ app/controllers/tourists_controller.rb
@@ -1,10 +1,6 @@
 class TouristsController < ApplicationController
     before_action :authenticate_user!
     def show
-        if current_tourist
-            @tourist = current_tourist
-        else
-            @tourist = Tourist.find(params[:id])
-        end
+        @tourist = Tourist.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :agencies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :agencies, only: :show
+  resources :agencies, only: [:index, :show] 
   resources :tourists , only: :show
   resources :reviews
   resources :tours
