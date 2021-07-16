2366657d226166cecc2424f81ed3143617555f83
Leif Bersamina <leifbersamina@gmail.com>
2021-07-16 20:48:41 +0800

    add comma

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: %i[type username first_name last_name address birth_date agency_name approved])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:profile_pic, :cover_pic, :address, :birth_date, :agency_name :approved ]) 
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:profile_pic, :cover_pic, :address, :birth_date, :agency_name, :approved ]) 
       end
 end
